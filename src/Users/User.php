dcbe64baa19d439e7b1bcea44442039c21d34ace
Diego Mota <diego.smota@gmail.com>
2020-04-17 11:06:17 +0100

    Fix a typo

--- src/Users/User.php
+++ src/Users/User.php
@@ -47,7 +47,7 @@ class User implements \JsonSerializable
     private $createdTimestamp;
 
     /**
-     * @Type("array<KeycloackAdmin\Credentials\CredentialRepresentation>")
+     * @Type("array<KeycloakAdmin\Credentials\CredentialRepresentation>")
      */
     private $credentials;
 
